ce4111a129a1486f89b245ff56c995a7378e030b
Wei Lim <weikanglim@gmail.com>
2023-05-11 20:50:36 -0700

    Clean up examples

--- src/lesson-1-ownership/src/main.rs
+++ src/lesson-1-ownership/src/main.rs
@@ -1,24 +1,30 @@
-fn main() {
-    let p1 = Person { age: 14, name: "Tosh".to_owned() };
-    let p2 = Person { age: 16, name: "Dendra".to_owned() };
+/*!
+In this lesson, we explore the basic ideas of ownership in Rust.
+*/
 
-    // Print person by passing ownership
-    print_person_by_ownership(p1);
+fn main() {
+    let p1 = Person { age: 14, name: String::from("Tosh") };
+    let p2 = Person { age: 16, name: String::from("Dendra") };
+
+    // Print person by passing ownership (i.e. move semantics)
+    print_person_owned(p1);
     // p1 no longer available after move
-    // print_person_by_ownership(p1) will be flagged by compiler
+    // print_person_owned(p1) will be flagged by compiler
 
     // Print person by borrow (using references)
     print_person(&p2);
     print_person(&p2); // still works
 
     // Print person by creating a separate deep-copy of person
-    print_person_by_ownership(p2.clone());
+    print_person_owned(p2.clone());
 
-    // Illustrates copy semantics using Clone (.clone) vs Copy trait (implciit)
+    // Simple implements the Copy trait. This changes the default semantics
+    // to be copy instead of move.
     let my_simple = Simple { integer: 10 };
 
-    print_simple(my_simple.clone());
     print_simple(my_simple);
+    print_simple(my_simple); // still valid
+    print_simple(my_simple.clone()); // equivalent to the previous two statements
 }
 
 fn print_simple(simple: Simple) {
@@ -29,7 +35,7 @@ fn print_person(person: &Person) {
     println!("Person with name: {}, age: {}", person.name, person.age);
 }
 
-fn print_person_by_ownership(person: Person) {
+fn print_person_owned(person: Person) {
     println!("Owned person with name: {}, age: {}", person.name, person.age);
 }
 
